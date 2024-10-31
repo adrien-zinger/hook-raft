1e481b85b83f3c78bb6f2151b2e4ce4309ee0ca5
Adrien Zinger <zinger.ad@gmail.com>
2024-10-31 16:35:18 +0100

    Try examples

--- example/hook/test/run.sh
+++ example/hook/test/run.sh
@@ -1,82 +1,61 @@
-subnet=255.255.255.248/29
-img=dokken/centos-stream-9
-volume=./:/servers
+# Store current network state (eno2 required)
+current_state=`ip -o -4 addr show dev eno2 | grep -oP '\d+\.\d+\.\d+\.\d+/\d+'`
+ip[1]=192.168.1.1
+ip[2]=192.168.1.2
+ip[3]=192.168.1.3
+ip[4]=192.168.1.4
+ip[5]=192.168.1.5
 
-net=hook-net
-
-flags="--network ${net} -dit -v ${volume}"
-
-ip1=255.255.255.250
-ip2=255.255.255.251
-ip3=255.255.255.252
-ip4=255.255.255.253
-ip5=255.255.255.254
-
-srv_port=8080
-
-cmd_pre="chmod +x commit_term && \
-	chmod +x switch_status && \
-	chmod +x append_term && \
-	chmod +x pre_append_term && \
-	chmod +x retreive_term && \
-	chmod +x retreive_n_term && \
-	chmod +x prepare_term"
+# Create a subinterface
+sudo ip link add link eno2 name eno2.10 type vlan id 10
+for i in `seq 5`; do
+	sudo ip addr add ${ip[$i]}/24 dev eno2.10
+done
+sudo ip link set eno2.10 up
 
 # Make settings
-i=1
-while test -le 5; do
+for i in `seq 5`; do
 	rm -f server$i/settings	> /dev/null
 	cat common_settings.toml 		>> server$i/settings.toml
 	cat server$i/server_settings.toml 	>> server$i/settings.toml
-	i=$((i + 1))
+
+	nodes="nodes = ["
+	for j in `seq 5`; do
+		if test $i -ne $j; then
+			nodes=$nodes\'$ip[$j]", "
+		fi
+	done
+	nodes=$nodes"]"
+	echo $nodes >> server$i/settings.toml
+	echo "addr = \"${ip[$i]}\""
+	
+	# Also set scripts as executable
+	chmod +x server$i/commit_term
+	chmod +x server$i/switch_status
+	chmod +x server$i/append_term
+	chmod +x server$i/pre_append_term
+	chmod +x server$i/retreive_term
+	chmod +x server$i/retreive_n_term
+	chmod +x server$i/prepare_term
 done
 
 timeout=10
 kill_timeout=5
-cmd1="cd /servers/server1 && \
-	${cmd_pre} && \
-	timeout ${kill_timeout} ../hook > output.log 2>&1"
-cmd2="cd /servers/server2 && \
-	${cmd_pre} && \
-	timeout ${kill_timeout} ../hook > output.log 2>&1"
-cmd3="cd /servers/server3 && \
-	${cmd_pre} && \
-	timeout ${timeout} ../hook > output.log 2>&1"
-cmd4="cd /servers/server4 && \
-	${cmd_pre} && \
-	timeout ${timeout} ../hook > output.log 2>&1"
-cmd5="cd /servers/server5 && \
-	${cmd_pre} && \
-	timeout ${timeout} ../hook > output.log 2>&1"
+
+# Run servers on childs bash
+bash -c "cd /servers/server1 && ${cmd_pre} && timeout ${kill_timeout} ../hook > output.log 2>&1" &
+bash -c "cd /servers/server2 && ${cmd_pre} && timeout ${kill_timeout} ../hook > output.log 2>&1" &
+bash -c "cd /servers/server3 && ${cmd_pre} && timeout ${kill_timeout} ../hook > output.log 2>&1" &
+bash -c "cd /servers/server4 && ${cmd_pre} && timeout ${kill_timeout} ../hook > output.log 2>&1" &
+bash -c "cd /servers/server5 && ${cmd_pre} && timeout ${kill_timeout} ../hook > output.log 2>&1" &
+
+echo "Test running..."
+sleep $timeout
 
 rm -f server*/*.log > /dev/null &2>1
 rm -f server*/term_* > /dev/null &2>1
-docker rm -f srv1 srv2 srv3 srv4 srv5 > /dev/null &2>1
 
-docker network ls | grep ${net} > /dev/null
-if test $? = 1; then
-	docker network create --subnet $subnet ${net}
-fi
-
-docker run $flags --ip $ip1 --expose $srv_port --name srv1 $img bash
-docker run $flags --ip $ip2 --expose $srv_port --name srv2 $img bash
-docker run $flags --ip $ip3 --expose $srv_port --name srv3 $img bash
-docker run $flags --ip $ip4 --expose $srv_port --name srv4 $img bash
-docker run $flags --ip $ip5 --expose $srv_port --name srv5 $img bash
-
-docker exec srv1 bash -c "${cmd1}" &
-docker exec srv2 bash -c "${cmd2}" &
-docker exec srv3 bash -c "${cmd3}" &
-docker exec srv4 bash -c "${cmd4}" &
-docker exec srv5 bash -c "${cmd5}" &
-
-docker logs srv1
-docker logs srv2
-docker logs srv3
-docker logs srv4
-docker logs srv5
-
-sleep $timeout
-
-echo clean containers
-docker rm -f srv1 srv2 srv3 srv4 srv5
+# Restore network configuration
+sudo ip link set eno2.10 down
+sudo ip link delete eno2.10
+sudo ip addr $current_state dev eno2
